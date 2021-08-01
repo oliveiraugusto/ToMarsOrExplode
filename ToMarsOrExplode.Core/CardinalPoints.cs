82290b6357e57f8d11fe6addf6ad05fdf0ed19b3
Cesar Oliveira Jr <contato@retrum.com.br>
2021-08-01 16:48:57 -0300

    Add project files.

---
+++ ToMarsOrExplode.Core/CardinalPoints.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToMarsOrExplode.Core
+{
+    public enum CardinalPoints
+    {
+        N, //norte
+        S, //sul
+        E, //leste
+        W //oeste
+    }
+}
---
+++ ToMarsOrExplode.Core/Grid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToMarsOrExplode.Core
+{
+    public struct Grid
+    {
+        public int height, width;
+
+        public Grid(int h, int w)
+        {
+            height =h;
+            width = w;
+        }
+    }
+}
